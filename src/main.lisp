7800070aaa443b88be80816752185822f222d381
Lewis Grozinger (Student) <b8051106@cypher214.ncl.ac.uk>
2019-11-05 16:32:20 +0000

    Some refactoring of WITH-ODE-SYSTEM and INTEGRATE

--- src/main.lisp
+++ src/main.lisp
@@ -13,7 +13,7 @@
 (use-foreign-library libgsl)
 
 ;; System definition as follows:
-;; ((variable . update-form) | (parameter . value))
+;; ((variable . update-form) | (parameter . numberp))
 
 ;; E.g. Lotka-Volterra
 (defvar *lotka-volterra* '(x (- (* ɑ x) (* β x y))
@@ -23,16 +23,45 @@
                            ɣ 10.0)
   "An example ODE system for Lotka-Volterra dynamics.")
 
-;; GSL stepping methods commented out are methods requiring Jacobian
+
+;; GSL CFFI DEFINITION
+
+;; GSL stepping methods
 (defcvar (*rk23* "gsl_odeiv2_step_rk2") :pointer)
 (defcvar (*rk4* "gsl_odeiv2_step_rk4") :pointer)
 (defcvar (*rk45* "gsl_odeiv2_step_rkf45") :pointer)
 (defcvar (*rkck* "gsl_odeiv2_step_rkck") :pointer)
 (defcvar (*rk8pd* "gsl_odeiv2_step_rk8pd") :pointer)
-;;(defvar *rk4imp* (defcvar "gsl_odeiv2_step_rk4imp"))
-;;(defvar *bsimp* (defcvar "gsl_odeiv2_step_bsimp"))
 (defcvar (*msadams* "gsl_odeiv2_step_msadams") :pointer)
-;;(defvar *msbdf* (defcvar "gsl_odeiv2_step_msbdf"))
+
+;; GSL ode system
+(defcstruct gsl-odeiv2-system
+  (function :pointer)
+  (jacobian :pointer)
+  (dimension :uint)
+  (params :pointer))
+
+(defctype odes (:struct gsl-odeiv2-system))
+
+;; GSL driver generator function
+(defcfun "gsl_odeiv2_driver_alloc_y_new" :pointer
+  (sys (:pointer odes))
+  (step-type :pointer)
+  (hstart :double)
+  (epsabs :double)
+  (epsrel :double))
+
+;; GSL driver application function
+(defcfun "gsl_odeiv2_driver_apply" :int
+  (d :pointer)
+  (time :pointer)
+  (next-time :double)
+  (y (:pointer :double)))
+
+;; GSL driver free function
+(defcfun "gsl_odeiv2_driver_free" :int
+  (d :pointer))
+
 
 (defun variables (in-system)
   "Collect the variables (including parameters) for the system."
@@ -59,117 +88,101 @@
   (loop for variable in (variables in-system)
      collect (equation variable in-system)))
 
-(defun doublef (number)
-  (coerce number 'double-float))
+(defun tree-substitute (new old tree)
+  "Replace (recursively) all instances of old with new in tree."
+  (flet ((replacer (x)
+           (cond ((eql x old) new)
+                 ((listp x) (tree-substitute new old x))
+                 (t x))))
+    (mapcar #'replacer tree)))
 
-;; low level CFFI for libgsl ordinary differential equations
-(defcstruct gsl-odeiv2-system
-  (function :pointer)
-  (jacobian :pointer)
-  (dimension :uint)
-  (params :pointer))
-
-;; GSL driver generator function
-(defcfun "gsl_odeiv2_driver_alloc_y_new" :pointer
-  (sys (:pointer (:struct gsl-odeiv2-system)))
-  (step-type :pointer)
-  (hstart :double)
-  (epsabs :double)
-  (epsrel :double))
-
-;; GSL driver application function
-(defcfun "gsl_odeiv2_driver_apply" :int
-  (d :pointer)
-  (time :pointer)
-  (next-time :double)
-  (y (:pointer :double)))
-
-;; GSL driver free function
-(defcfun "gsl_odeiv2_driver_free" :int
-  (d :pointer))
-
-(defun symbol-to-mem-aref (form symbol pointer index)
-  "Replace all occurrences of symbol in form with (mem-aref pointer :double index)."
-  (cond ((and (symbolp form) (eql form symbol))
-         (list 'mem-aref pointer :double index))
-        ((listp form)
-         (loop for sub-form in form
-            collect (symbol-to-mem-aref sub-form symbol pointer index)))
-        (t form)))
-
-(defun system-to-mem-aref (system y-pointer p-pointer)
-  "Replace occurrences of symbols with appropriate mem-aref forms."
+(defun substitute-variables (variables array-pointer system)
   (let ((result system)
-        (ys (dependent-variables system))
-        (ps (parameters system)))
-    (loop for y in ys for i from 0 below (length ys)
-       do (setf result (loop for form in result
-                          collect (symbol-to-mem-aref form y y-pointer i))))
-    (loop for p in ps for i from 0 below (length ps)
-       do (setf result (loop for form in result
-                       collect (symbol-to-mem-aref form p p-pointer i))))
+        (dimension (length variables)))
+    
+    (flet ((array-ref (i) (list 'mem-aref array-pointer :double i)))
+      (loop for variable in variables for i below dimension
+         do (setf result (tree-substitute (array-ref i) variable result))))
 
     result))
 
-(defun nested-replace (new old sequence)
-  (cond ((eql sequence old) new)
-        ((listp sequence) (loop for sub in sequence collect (nested-replace new old sub)))
-        (t sequence)))
-
-(defmacro defode ((name) &body system-definition)
-  "Construct a C CFFI callback to be used for GSL ODE methods that implements the system definition."
+(defmacro defode ((name) &body system)
+  "Define a CFFI callback for the ODE system."
   (with-gensyms (time y dydt params)
-    (let ((transformed-system (system-to-mem-aref system-definition y params)))
-      `(defcallback ,name :int ((,time :double)
-                                (,y (:pointer :double))
-                                (,dydt (:pointer :double))
-                                (,params :pointer))
-         (declare (ignorable ,time ,params))
-         ,@(loop for (variable update) on transformed-system by #'cddr
-              when (member y variable)
-              collect (list 'setf (nested-replace dydt y variable) update))
+    ;; replace dependent-variables with mem-aref forms
+    (setf system (substitute-variables (dependent-variables system) y system))
 
-         0))))
+    ;; replace parameters with mem-aref forms
+    (setf system (substitute-variables (parameters system) params system))
+
+    ;; replace occurence of the symbol TIME with the gensym'ed symbol
+    (setf system (tree-substitute time 'TIME system))
+
+    ;; here is the callback definition
+    `(defcallback ,name :int ((,time :double)
+                              (,y (:pointer :double))
+                              (,dydt (:pointer :double))
+                              (,params :pointer))
+       (declare (ignorable ,time ,params))
+       
+       ;; convert the system to a progn of SETF forms
+       ,@(loop for (lhs rhs) on system by #'cddr when (member y lhs)
+            collect (list 'setf (substitute dydt y lhs) rhs))
+
+       ;; finally return the GSL_SUCCESS code
+       0)))
 
 (defmacro with-ode-system ((name &rest system) &body body)
   "Construct a gsl_odeiv2_system for system, bind to name and execute body."
   (with-gensyms (foreign-params odefun)
+    (let ((params (parameters system)))
     `(progn
        (defode (,odefun) ,@system)
-       (with-foreign-objects ((,name '(:struct gsl-odeiv2-system)) (,foreign-params :double ,(length (parameters system))))
-         ;; populate the foreign allocated  parameter array
-         ,@(loop for i from 0 below (length (parameters system)) for p in (parameters system)
-              collect (list 'setf (list 'mem-aref foreign-params :double i) (doublef (getf system p))))
+       (with-foreign-objects ((,name '(:struct gsl-odeiv2-system))
+                              (,foreign-params :double ,(length params)))
+         ;; populate the foreign allocated parameter array
+         ,@(loop for param in params for i below (length params)
+              collect
+                (list 'setf
+                      (list 'mem-aref foreign-params :double i)
+                      (coerce (getf system param) 'double-float)))
          
-         ;; set the ODE callback and Jacobian for GSL ODE system foreign struct              
-         (setf (foreign-slot-value ,name '(:struct gsl-odeiv2-system) 'function) (callback ,odefun)
-               (foreign-slot-value ,name '(:struct gsl-odeiv2-system) 'jacobian) (null-pointer)
-               (foreign-slot-value ,name '(:struct gsl-odeiv2-system) 'dimension) ,(length (dependent-variables system))
-               (foreign-slot-value ,name '(:struct gsl-odeiv2-system) 'params) ,foreign-params)
-
-         ,@body))))
+         ;; set the ODE callback and Jacobian for GSL ODE system foreign struct
+         (with-foreign-slots ((function jacobian dimension params)
+                              ,name (:struct gsl-odeiv2-system))
+           
+           (setf function (callback ,odefun)
+                 jacobian (null-pointer)
+                 dimension ,(length (dependent-variables system))
+                 params ,foreign-params))
+           
+         ,@body)))))
 
 (defun integrate (gsl-system initial-state
                   &key
                     (out-file t)
                     (from-to (cons 0.d0 1.d1))
                     (steps 100)
-                    (method *rk45*))
+                    (method *rk8pd*)
+                    (abserr 1.d-6)
+                    (relerr 0.d0))
 
-  (let ((dimension (foreign-slot-value gsl-system '(:struct gsl-odeiv2-system) 'dimension)))
-    (with-foreign-objects ((y :double dimension) (time :double))
-      (let* ((start (doublef (car from-to)))
-             (end (doublef (cdr from-to)))
+  (let ((d (foreign-slot-value gsl-system 'odes 'dimension)))
+    (with-foreign-objects ((y :double d) (time :double))
+      (let* ((start (coerce (car from-to) 'double-float))
+             (end (coerce (cdr from-to) 'double-float))
              (step-size (/ (- end start) steps))
              (driver (gsl-odeiv2-driver-alloc-y-new
                       gsl-system
                       method
                       1.d-6
-                      1.d-6
-                      0.d0)))
+                      abserr
+                      relerr)))
 
         ;; set the initial state in the foreign array y
-        (loop for i from 0 below dimension do (setf (mem-aref y :double i) (doublef (elt initial-state i))))
+        (loop for i below d
+           do (setf (mem-aref y :double i)
+                    (coerce (elt initial-state i) 'double-float)))
 
         ;; set the initial time
         (setf (mem-ref time :double) start)
@@ -178,16 +191,20 @@
         (loop
            for next-time = (+ start step-size)
            then (+ (mem-ref time :double) step-size)
-           until (> next-time end) 
+           with status = 0
+           until (or (> next-time end) (not (eql status 0)))
            do
-             (let ((status (gsl-odeiv2-driver-apply driver time next-time y)))
-               (if (not (eql status 0))
-                   (format t "~&The integrator failed with status ~A~%" status)
-                   (format out-file "~&~,6f ~{~,6f~^ ~}~%"
-                           (mem-ref time :double)
-                           (loop for i below dimension collect (mem-aref y :double i))))))
+             (setf status (gsl-odeiv2-driver-apply driver time next-time y))
 
-        (gsl-odeiv2-driver-free driver)))))
+             (case status
+               (0 (format out-file "~&~,6f ~{~,6f~^ ~}~%"
+                          (mem-ref time :double)
+                          (loop for i below d collect (mem-aref y :double i))))
+               (otherwise (format t "~&The integrator failed with status ~A~%"
+                                  status)
+                          (return)))
+
+           finally (gsl-odeiv2-driver-free driver))))))
              
              
       
